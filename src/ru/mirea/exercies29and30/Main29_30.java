42dea55af2b035efb31295c6bb15a53b6e05a881
ARAM <aramkenny97@gmail.com>
2020-12-20 01:49:23 +0300

    Done

---
+++ src/ru/mirea/exercies29and30/Main29_30.java
@@ -0,0 +1,8 @@
+package ru.mirea.exercies29and30;
+
+public class Main29_30 {
+    public static void main(String[] args) {
+        StreamAPI streamAPI = new StreamAPI();
+        streamAPI.words();
+    }
+}
